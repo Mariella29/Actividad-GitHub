67e8f977f8c978daba1ebac1546a17641d74c63c
mariella mendez <mendezmariella11@gmail.com>
2023-06-20 01:02:06 -0600

    holi

--- js/actividad.js
+++ js/actividad.js
@@ -3,3 +3,13 @@ function determinarTipoTriangulo (ladoA, ladoB, LadoC){
         return "Triángulo equilátero";
     } else if (ladoA === ladoB || ladoA === ladoC || ladoB === ladoC)
 }
+return "Triángulo Isósceles";
+{
+    else {
+        return "Triángulo escaleno";
+} 
+}
+
+console.log(determinarTipoTriangulo(5, 5, 5));
+console.log(determinarTipoTriangulo(6, 6, 5));
+console.log(determinarTipoTriangulo(4, 5, 3));
